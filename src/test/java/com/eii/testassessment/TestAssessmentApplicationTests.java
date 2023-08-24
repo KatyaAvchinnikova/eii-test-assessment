8df30b0ce24d2be01dc981307b2ab9de70885c64
Kate Auchynnikava <avchinnikova.katya@gmail.com>
2023-08-24 09:48:36 -0300

    initial commit

---
+++ src/test/java/com/eii/testassessment/TestAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eii.testassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
